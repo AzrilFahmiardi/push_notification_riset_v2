79902ca458e419a10aed272ae8a015b78c2bfdfa
AzrilFahmiardi <anakijummira@gmail.com>
2025-04-21 13:00:07 +0700

    masih env

--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -1,5 +1,7 @@
 import admin from 'firebase-admin';
 // import serviceAccount from './serviceAccountKey.json';
+require('dotenv').config(); // pastikan dotenv dipanggil dulu
+
 
 let serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
