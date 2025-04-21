87f36526b0ce3ab88629a5d9e61ea27bbbae15df
AzrilFahmiardi <anakijummira@gmail.com>
2025-04-21 12:46:41 +0700

    atur environtment

--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -1,7 +1,7 @@
 import admin from 'firebase-admin';
 // import serviceAccount from './serviceAccountKey.json';
 
-let serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+let serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 
 if (!admin.apps.length) {
