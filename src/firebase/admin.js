348be121275bd7fab53b3c97892004601e65e35f
AzrilFahmiardi <anakijummira@gmail.com>
2025-04-21 12:43:05 +0700

    ganti environtment

--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -1,5 +1,8 @@
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json';
+// import serviceAccount from './serviceAccountKey.json';
+
+let serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+
 
 if (!admin.apps.length) {
   admin.initializeApp({
