e38f939a2ad9bc145135a73cf99346622e7e3782
AzrilFahmiardi <anakijummira@gmail.com>
2025-04-21 15:20:46 +0700

    update firebase admin

--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -1,10 +1,10 @@
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json';
-// import 'dotenv/config';
+// import serviceAccount from './serviceAccountKey.json';
+import 'dotenv/config';
 
-// const serviceAccount = JSON.parse(
-//   Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf-8')
-// );
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf-8')
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
